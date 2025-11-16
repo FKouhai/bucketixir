4b2aad6e43dbb5c4e10bf46e9026e32558d59704
FKouhai <frandres00@gmail.com>
2025-11-16 12:17:45 +0100

    refactor: add functions that can be re-usable

--- lib/bucketixir/s3_client.ex
+++ lib/bucketixir/s3_client.ex
@@ -7,31 +7,14 @@ defmodule Bucketixir.S3Client do
   Makes an S3 request using ExAws.
 
   For list buckets, bucket is nil.
+  For list objects, bucket is not nil.
   """
   @spec s3_request(String.t(), String.t(), map(), String.t() | nil) ::
           {:ok, map()} | {:error, term()}
   def s3_request(endpoint, region, config, bucket) do
     # Set ExAws config globally for this request
     Application.put_env(:ex_aws, :debug_requests, true)
-    Application.put_env(:ex_aws, :access_key_id, config.access_key_id)
-    Application.put_env(:ex_aws, :secret_access_key, config.secret_access_key)
-    # Application.put_env(:ex_aws, :region, region)
-
-    uri = URI.parse(endpoint)
-
-    ex_aws_config =
-      ExAws.Config.new(:s3)
-      # 2. Configure the custom endpoint for connection
-      |> Map.put(:endpoint, %{
-        host: uri.host,
-        scheme: uri.scheme,
-        port: uri.port
-        # path_style: true
-      })
-      |> Map.put(:region, region)
-      |> Map.put(:signature_version, :v4)
-      # Use the argument region for signing
-      |> Map.put(:aws_signing_region, region)
+    ex_aws_config = set_config(endpoint, region, config)
 
     # For list buckets, bucket is nil
     if bucket == nil do
@@ -43,5 +26,28 @@ defmodule Bucketixir.S3Client do
       |> Map.put(:parser, fn body -> body end)
       |> ExAws.request(ex_aws_config)
     end
+  end
+
+  @doc """
+  set_config auxiliary setter for configuration
+  returns the ex_aws_config
+  """
+  def set_config(endpoint, region, config) do
+    Application.put_env(:ex_aws, :access_key_id, config.access_key_id)
+    Application.put_env(:ex_aws, :secret_access_key, config.secret_access_key)
+    uri = URI.parse(endpoint)
+
+    ExAws.Config.new(:s3)
+    # 2. Configure the custom endpoint for connection
+    |> Map.put(:endpoint, %{
+      host: uri.host,
+      scheme: uri.scheme,
+      port: uri.port,
+      path: uri.path
+    })
+    |> Map.put(:region, region)
+    |> Map.put(:signature_version, :v4)
+    # Use the argument region for signing
+    |> Map.put(:aws_signing_region, region)
   end
 end
