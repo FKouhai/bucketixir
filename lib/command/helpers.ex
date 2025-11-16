701d64ce1f6e66f43f6d6b6f12a911b10c90b006
FKouhai <frandres00@gmail.com>
2025-11-16 12:22:16 +0100

    feat: add helpers to reduce code duplication

---
+++ lib/command/helpers.ex
@@ -0,0 +1,23 @@
+defmodule Bucketixir.Command.Helpers do
+  @moduledoc """
+  Helper functions and methods to remove code duplication
+  """
+  @config_file Path.join(System.user_home!(), ".bucketixir.yaml")
+  # Uses YamlElixir to read the credentials from the config file
+  # Then it stores them into a map
+  @doc """
+  load_config public helper that reads the config file and stores it into memory
+  """
+  def load_config do
+    case YamlElixir.read_from_file(@config_file, []) do
+      {:ok, %{"credentials" => credentials}} when is_map(credentials) ->
+        {:ok, credentials |> Enum.into(%{}, fn {k, v} -> {String.to_atom(k), v} end)}
+
+      {:ok, _} ->
+        {:error, "config file found but credentials structure is invalid"}
+
+      {:error, reason} ->
+        {:error, "failed to parse yaml: #{inspect(reason)}"}
+    end
+  end
+end
